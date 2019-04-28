7ed03846de0c2000ff6a3f32b8ea72650ba36182
AregevDev <aregevdev@gmail.com>
2019-04-28 22:56:42 +0300

    Link the necessary frameworks on MacOS

--- src/raylib-cr/link.cr
+++ src/raylib-cr/link.cr
@@ -1,7 +1,7 @@
 macro link_flag
     {% if flag?(:linux) && flag?(:x86_64) %}
-        @[Link(ldflags: "-L#{__DIR__}/../native/linux -lraylib -lm -lpthread -ldl -lrt -lX11")]
+        @[Link(ldflags: "-L#{__DIR__}/../../native/linux -lraylib -lm -lpthread -ldl -lrt -lX11")]
     {% elsif flag?(:darwin) && flag?(:x86_64) %}
-        @[Link(ldflags: "-L#{__DIR__}/../native/darwin -lraylib")]
+        @[Link(ldflags: "-L#{__DIR__}/../../native/darwin -lraylib -framework OpenGL -framework Cocoa -framework IOKit -framework CoreFoundation -framework CoreVideo")]
     {% end %}
 end
