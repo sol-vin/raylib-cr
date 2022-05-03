ef3db607bac7454457f9a8130fc3b60db48e07f0
sol.vin <ian@sol.vin>
2022-05-03 01:55:58 -0700

    More raygui

--- src/raylib-cr/lib_raygui.cr
+++ src/raylib-cr/lib_raygui.cr
@@ -135,7 +135,7 @@ lib LibRayGUI
   fun fade = GuiFade(alpha : LibC::Float)
   fun set_state = GuiSetState(state : LibC::Int)
   fun get_state = GuiSetState : LibC::Int
-  
+
   fun set_font = GuiSetFont(font : LibRaylib::Font)
   fun get_font = GuiGetFont : LibRaylib::Font
 
@@ -155,11 +155,21 @@ lib LibRayGUI
   fun toggle_group = GuiToggleGroup(bounds : LibRaylib::Rectangle, text : LibC::Char*, active : LibC::Int) : LibC::Int
   fun check_box = GuiCheckBox(bounds : LibRaylib::Rectangle, text : LibC::Char*, checked : Bool) : Bool
   fun combo_box = GuiComboBox(bounds : LibRaylib::Rectangle, text : LibC::Char*, active : LibC::Int) : LibC::Int
-  
+  fun dropdown_box = GuiDropdownBox(bounds : LibRaylib::Rectangle, text : LibC::Char*, active : LibC::Int*, edit_mode : Bool) : Bool
+  fun spinner = GuiSpinner(bounds : LibRaylib::Rectangle, text : LibC::Char*, value : LibC::Int*, min_value : LibC::Int, max_value : LibC::Int, edit_mode : Bool) : Bool
+  fun value_box = GuiValueBox(bounds : LibRaylib::Rectangle, text : LibC::Char*, value : LibC::Int*, min_value : LibC::Int, max_value : LibC::Int, edit_mode : Bool) : Bool
+  fun text_box = GuiTextBox(bounds : LibRaylib::Rectangle, text : LibC::Char*, text_size : LibC::Int, edit_mode : Bool) : Bool
+  fun text_box_multi = GuiTextBoxMulti(bounds : LibRaylib::Rectangle, text : LibC::Char*, text_size : LibC::Int, edit_mode : Bool) : Bool
+  fun slider = GuiSlider(bounds : LibRaylib::Rectangle, text_left : LibC::Char*, text_right : LibC::Char*, value : LibC::Float, min_value : LibC::Float, max_value : LibC::Float) : LibC::Float
+  fun slider_bar = GuiSliderBar(bounds : LibRaylib::Rectangle, text_left : LibC::Char*, text_right : LibC::Char*, value : LibC::Float, min_value : LibC::Float, max_value : LibC::Float) : LibC::Float
+  fun progress_bar = GuiProgressBar(bounds : LibRaylib::Rectangle, text_left : LibC::Char*, text_right : LibC::Char*, value : LibC::Float, min_value : LibC::Float, max_value : LibC::Float) : LibC::Float
+  fun status_bar = GuiStatusBar(bounds : LibRaylib::Rectangle, text : LibC::Char*)
+  fun dummy_rec = GuiDummyRec(bounds : LibRaylib::Rectangle, text : LibC::Char*)
+  fun scroll_bar = GuiScrollBar(bounds : LibRaylib::Rectangle, value : LibC::Int, min_value : LibC::Int, max_value : LibC::Int) : LibC::Int
+  fun grid = GuiGrid(bounds : LibRaylib::Rectangle, spacing : LibC::Float, subdivs : LibC::Int) : LibRaylib::Vector2
 
-
-
-
-
-
+  fun list_view = GuiListView(bounds : LibRaylib::Rectangle, text : LibC::Char*, scroll_index : LibC::Int*, active : LibC::Int) : LibC::Int
+  fun list_view_ex = GuiListView(bounds : LibRaylib::Rectangle, text : LibC::Char**, count : LibC::Int, focus : LibC::Int*, scroll_index : LibC::Int*, active : LibC::Int) : LibC::Int
+  fun message_box = GuiMessageBox(bounds : LibRaylib::Rectangle, title : LibC::Char*, message : LibC::Char*, buttons : LibC::Char*) : LibC::Int
+  fun text_input_box = GuiTextInputBox(bounds : LibRaylib::Rectangle, title : LibC::Char*, message : LibC::Char*, buttons : LibC::Char*, text : LibC::Char*) : LibC::Int
 end
