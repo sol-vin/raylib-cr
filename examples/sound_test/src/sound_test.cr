75cee3fa20403c58f7f30dd546c2773d4cd89c7f
Ian Rash <redcodefinal@gmail.com>
2022-10-24 15:20:25 -0700

    it actually worked the whole time i just suck

--- examples/sound_test/src/sound_test.cr
+++ examples/sound_test/src/sound_test.cr
@@ -1,6 +1,6 @@
 require "raylib-cr"
 
-alias Rl = LibRayli
+alias Rl = LibRaylib
 
 puts "ma_data_converter_size: #{Rl.ma_data_converter_size}"
 puts "ma_device_size: #{Rl.ma_device_size}"
@@ -8,7 +8,7 @@ puts "ma_context_size: #{Rl.ma_context_size}"
 puts "ma_mutex_size: #{Rl.ma_mutex_size}"
 
 Rl.init_window(100, 100, "Sound Test")
-
+Rl.init_audio_device
 Rl.set_target_fps(60)
 
 sound = Rl.load_sound("rsrc/marmapurr.mp3")
@@ -21,5 +21,5 @@ until Rl.close_window?
   Rl.clear_background(Rl::RAYWHITE)
   Rl.end_drawing
 end
-
+Rl.close_audio_device
 Rl.close_window
