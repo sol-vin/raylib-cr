81915fdf704d7d84554b44e1f267baa472fd4bb0
Ian Rash <redcodefinal@gmail.com>
2023-01-08 03:46:51 -0800

    Delete

--- examples/wireland/spec/palette_spec.cr
+++
@@ -1,8 +0,0 @@
-require "./spec_helper"
-
-describe Wireland::Pallette do
-  it "should be able to read a palette in" do
-    palette = Wireland::Pallette.new("rsrc/wireland.pal")
-    palette = Wireland::Pallette.new("rsrc/wireland-alt.pal")
-  end
-end
--- examples/wireland/src/wireland/components/join.cr
+++
@@ -1,9 +0,0 @@
-class Wireland::Component::Join < Wireland::Component
-  def self.allow_adjacent?
-    true
-  end
-
-  def self.output_whitelist
-    super.reject { |c| c == self }
-  end
-end
