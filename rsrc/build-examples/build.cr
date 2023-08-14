c072c25a0bb4338e2445589748d5529e7dcaa53c
Ian Rash <redcodefinal@gmail.com>
2023-08-13 20:51:33 -0700

    Rebuild examples on every push/pull request

---
+++ rsrc/build-examples/build.cr
@@ -0,0 +1,24 @@
+require "file_utils"
+
+FileUtils.cd("examples") do 
+  puts "BUILDING EXAMPLE FROM #{FileUtils.pwd}"
+  FileUtils.rm_r("_build")
+  FileUtils.mkdir_p("_build/rsrc")
+  Dir.entries(FileUtils.pwd).each do |name|
+    path = Path[name]
+    next if path.basename =~ /^\.{1,2}$/ || path.basename =~ /^_build$/
+
+    puts "BUILDING EXAMPLE #{path.basename}"
+
+    FileUtils.cd(path) do
+      `shards install`
+      `crystal build --release -s -p -t -o ../_build/#{path.basename} ./src/#{path.basename}.cr`
+      exit(1) unless File.exists?("../_build/#{name}.exe")
+      FileUtils.rm("../_build/#{name}.pdb")
+      begin
+        FileUtils.cp_r("./rsrc", "../_build/rsrc")
+      rescue
+      end
+    end
+  end
+end
