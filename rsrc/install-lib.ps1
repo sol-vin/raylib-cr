3791200278b7e08a2eb776d03f0ff0fa58fc6fc6
Ian Rash <redcodefinal@gmail.com>
2023-09-02 17:20:55 -0700

    Install lib for windows

---
+++ rsrc/install-lib.ps1
@@ -0,0 +1,20 @@
+cl /c lib\raylib-cr\rsrc\miniaudio-tests\miniaudiohelpers.c
+lib miniaudiohelpers.obj
+copy miniaudiohelpers.lib C:\raylib\miniaudiohelpers.lib
+del miniaudiohelpers.obj
+del miniaudiohelpers.lib
+
+git clone https://github.com/raysan5/raylib
+cd raylib
+mkdir build
+cd build 
+cmake .. -DBUILD_SHARED_LIBS=ON
+cmake --build .
+mkdir C:\raylib
+copy raylib\Debug\raylib.lib C:\raylib\raylib.lib
+copy raylib\Debug\raylib.dll C:\raylib\raylib.dll
+cd ..\..
+del raylib
+
+set PATH=%PATH%;C:\raylib\lib
+set LIB=%LIB%;C:\raylib\lib
