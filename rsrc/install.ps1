315c93143cf4306625f1465a7605d7a1f7d30c79
D. Shwagginz <mynameisformat@gmail.com>
2023-09-14 16:34:39 -0700

    Update install.ps1

--- rsrc/install.ps1
+++ rsrc/install.ps1
@@ -14,6 +14,16 @@ mkdir C:\raylib
 copy raylib\Debug\raylib.lib C:\raylib\raylib.lib
 copy raylib\Debug\raylib.dll C:\raylib\raylib.dll
 cd ..\..
+
+git clone https://github.com/raysan5/raygui
+cd raygui
+copy src\raygui.h src\raygui.c
+cl /O2 /I../raylib/src/ /D_USRDLL /D_WINDLL /DRAYGUI_IMPLEMENTATION /DBUILD_LIBTYPE_SHARED src/raygui.c /LD /Feraygui.dll /link /LIBPATH ../raylib/build/raylib/Debug/raylib.lib /subsystem:windows /machine:x64
+copy raygui.lib C:\raylib\raygui.lib
+copy raygui.dll C:\raylib\raygui.dll
+cd ..\..
+
+del raygui
 del raylib
 
 set PATH=%PATH%;C:\raylib\lib
