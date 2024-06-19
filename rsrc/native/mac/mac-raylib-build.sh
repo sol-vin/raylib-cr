4af0bb2ee41549e4fcf75a27838bb6ac6d208b17
Avram Eisner <avrame@gmail.com>
2024-06-18 20:57:29 -0700

    Mac Support Fixed Again (#37)
    - updated mac-raylib-build.sh to cd into the raylib repo root directory before creating the build sub-directory and running cmake and make
    - Added installation instructions for MacOS to the readme

--- rsrc/native/mac/mac-raylib-build.sh
+++ rsrc/native/mac/mac-raylib-build.sh
@@ -4,6 +4,7 @@ set -e
 
 cd $(dirname "$0")
 test -d raylib || git clone --depth 1 --branch 5.0 --recursive https://github.com/raysan5/raylib 
+cd raylib
 test -d build || mkdir build
 cd build
 cmake ..
--- rsrc/native/mac/mac-raylib-install.sh
+++ rsrc/native/mac/mac-raylib-install.sh
@@ -3,4 +3,5 @@
 set -e
 
 test -f $(dirname "$0")/raylib/build/raylib/libraylib.a || sh $(dirname "$0")/mac-raylib-build.sh
+test -d /usr/local/lib || mkdir /usr/local/lib
 sudo cp $(dirname "$0")/raylib/build/raylib/libraylib.a /usr/local/lib/libraylib.a
