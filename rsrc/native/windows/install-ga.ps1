c6d3ff796ed772623bb7722849e485f378e33297
Bar Hofesh <bar.hofesh@gmail.com>
2024-12-19 22:10:41 +0200

    Move to Release builds (#42)
    Thanks @bararchy!
    
    * Move to Release builds
    
    * all changes
    
    * don't build shared, this way we pull the dll into the exe
    
    * Release dir name
    
    * maybe we need shared?

--- rsrc/native/windows/install-ga.ps1
+++ rsrc/native/windows/install-ga.ps1
@@ -9,16 +9,16 @@ git clone --depth 1 --branch 5.0 https://github.com/raysan5/raylib
 cd raylib
 mkdir build
 cd build 
-cmake .. -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release
-cmake --build .
-copy raylib\Debug\raylib.lib ..\..\libs\raylib.lib
-copy raylib\Debug\raylib.dll ..\..\libs\raylib.dll
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
+cmake --build . --config Release
+copy raylib\Release\raylib.lib ..\..\libs\raylib.lib
+copy raylib\Release\raylib.dll ..\..\libs\raylib.dll
 cd ..\..
 
 git clone --depth 1 --branch 4.0 https://github.com/raysan5/raygui
 cd raygui
 copy src\raygui.h src\raygui.c
-cl /O2 /I../raylib/src/ /D_USRDLL /D_WINDLL /DRAYGUI_IMPLEMENTATION /DBUILD_LIBTYPE_SHARED src/raygui.c /LD /Feraygui.dll /link /LIBPATH ../raylib/build/raylib/Debug/raylib.lib /subsystem:windows /machine:x64
+cl /O2 /I../raylib/src/ /D_USRDLL /D_WINDLL /DRAYGUI_IMPLEMENTATION /DBUILD_LIBTYPE_SHARED src/raygui.c /LD /Feraygui.dll /link /LIBPATH ../raylib/build/raylib/Release/raylib.lib /subsystem:windows /machine:x64
 copy raygui.lib ..\libs\raygui.lib
 copy raygui.dll ..\libs\raygui.dll
 cd ..
