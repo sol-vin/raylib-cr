29fe26db5e529454376c2bb924f1d99a0e58a809
D. Shwagginz <mynameisformat@gmail.com>
2026-06-11 06:54:29 -0700

    6.0 Update (#51)
    * Fixed typo in rlgl_solar_system
    
    * updated readme and sh scripts
    
    * readme
    
    * windows install update
    
    * windows fix
    
    * 6.0 upload
    
    * fix
    
    * fix
    
    * fixes
    
    * install scrips
    
    * Updated all examples shards, updated raylib shards version, fixed library link for mac, finished install scrips (tested and used)
    
    * mac moment
    
    * dsstore
    
    * Delete .DS_Store
    
    * Delete rsrc/.DS_Store
    
    * Delete rsrc/native/.DS_Store
    
    * Delete rsrc/native/mac/.DS_Store

--- rsrc/native/mac/mac-raylib-uninstall.sh
+++ rsrc/native/mac/mac-raylib-uninstall.sh
@@ -3,3 +3,6 @@
 set -e
 
 sudo rm /usr/local/lib/libraylib.a
+sudo rm /usr/local/lib/libraylib.6.0.0.dylib
+sudo rm /usr/local/lib/libraylib.600.dylib
+sudo rm /usr/local/lib/libraylib.dylib
--- rsrc/native/ubuntu/install-lib.sh
+++ rsrc/native/ubuntu/install-lib.sh
@@ -6,14 +6,14 @@ sudo cp /usr/local/lib/libminiaudiohelpers.so /usr/lib/libminiaudiohelpers.so
 sudo ln -s /usr/lib/libminiaudiohelpers.so /lib/miniaudiohelpers.so
 
 # Compile raylib
-git clone --depth 1 --branch 5.0 https://github.com/raysan5/raylib
+git clone --depth 1 --branch 6.0 https://github.com/raysan5/raylib
 mkdir raylib/build
 sudo apt-get install libasound2-dev libx11-dev libxrandr-dev libxi-dev libgl1-mesa-dev libglu1-mesa-dev libxcursor-dev libxinerama-dev
 cmake raylib -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -B raylib/build 
 cmake --build raylib/build
 sudo make install -C raylib/build
-sudo cp /usr/local/lib/libraylib.so.5.0.0 /usr/lib/libraylib.so.500
-sudo ln -s /usr/lib/libraylib.so.500 /lib/raylib.so
+sudo cp /usr/local/lib/libraylib.so.6.0.0 /usr/lib/libraylib.so.600
+sudo ln -s /usr/lib/libraylib.so.600 /lib/raylib.so
 
 # Compile raygui
 git clone --depth 1 --branch 4.0 https://github.com/raysan5/raygui
