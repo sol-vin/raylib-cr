6e4506d58a2bb07de5ae94f8f43d840551e8cf82
D. Shwagginz <mynameisformat@gmail.com>
2026-06-12 09:52:57 -0700

    Working Raygui mac install scripts (#52)
    * Fixed typo in rlgl_solar_system
    
    * updated readme and sh scripts
    
    * readme
    
    * windows install update
    
    * windows fix
    
    * 6.0 upload
    
    * fix
    
    * fix
    
    * fixes
    
    * install scrips
    
    * Updated all examples shards, updated raylib shards version, fixed library link for mac, finished install scrips (tested and used)
    
    * mac moment
    
    * dsstore
    
    * Delete .DS_Store
    
    * Delete rsrc/.DS_Store
    
    * Delete rsrc/native/.DS_Store
    
    * Delete rsrc/native/mac/.DS_Store
    
    * working raygui
    
    * readme update for mac

--- rsrc/native/mac/mac-raylib-install.sh
+++ rsrc/native/mac/mac-raylib-install.sh
@@ -4,9 +4,11 @@ set -e
 
 test -d /usr/local/lib || mkdir /usr/local/lib
 
+# Raylib
 test -d raylib || git clone --depth 1 --branch 6.0 --recursive https://github.com/raysan5/raylib 
 cd raylib
 
+# Build static library
 test -d build || mkdir build
 cd build
 cmake .. -DCMAKE_POLICY_VERSION_MINIMUM=3.5
@@ -16,6 +18,7 @@ sudo cp ./raylib/libraylib.a /usr/local/lib/libraylib.a
 cd ..
 sudo rm -r ./build
 
+# Build shared library
 test -d build || mkdir build
 cd build
 cmake .. -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DBUILD_SHARED_LIBS=ON
@@ -25,4 +28,23 @@ sudo cp ./raylib/libraylib.600.dylib /usr/local/lib/libraylib.600.dylib
 sudo cp ./raylib/libraylib.dylib /usr/local/lib/libraylib.dylib
 
 cd ../../
+
+# Raygui
+test -d raygui || git clone --depth 1 --recursive https://github.com/raysan5/raygui
+cd raygui
+cp src/raygui.h src/raygui.c
+export PKG_CONFIG_PATH="../raylib/build/raylib"
+
+# Build static library
+gcc -c -o raygui.o src/raygui.c -I../raylib/build/raylib/include -DRAYGUI_IMPLEMENTATION $(pkg-config --cflags raylib)
+ar rcs libraygui.a raygui.o
+sudo cp ./libraygui.a /usr/local/lib/libraygui.a
+
+# Build shared library
+gcc -o raygui.dylib src/raygui.c -I../raylib/build/raylib/include -shared -fpic -DRAYGUI_IMPLEMENTATION -framework OpenGL -lm -lpthread -ldl $(pkg-config --libs --cflags raylib)
+sudo cp ./raygui.dylib /usr/local/lib/raygui.dylib
+
+# Cleanup
+cd ../
 sudo rm -r raylib
+sudo rm -r raygui
